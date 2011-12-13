a323bbc5b3950de1ceeac101baee6f5a3737a4d4
asanchez <asanchez@plutec.net>
2011-12-14 00:52:51 +0100

    Añadido e implementado en RatingC el método getBestRatedFilms

--- MFF/src/java/MFF/Controller/RatingController.java
+++ MFF/src/java/MFF/Controller/RatingController.java
@@ -5,6 +5,9 @@
 
 package MFF.Controller;
 
+import MFF.Model.Film;
+import MFF.Model.RSManagement;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
@@ -13,7 +16,10 @@ import java.util.HashMap;
  * @date 13-dic-2011
  */
 public class RatingController implements ControllerInterface {
-
+    RSManagement model;
+    public RatingController() {
+	model=new RSManagement();
+    }
     @Override
     public HashMap<String, Object> call(String action, HashMap<String, Object> parameters) {
 	if (action.equals("rate")) {
@@ -29,9 +35,15 @@ public class RatingController implements ControllerInterface {
 	return null;
     }
     protected HashMap<String, Object> editRate(HashMap<String, Object> parameters) {
+	
 	return null;
     }
     protected HashMap<String, Object> getBestRatedFilms(HashMap<String, Object> parameters) {
-	return null;
+	HashMap<String, Object> toRet=new HashMap<String, Object>();
+	int max=10; //Máximo de películas a devolver
+	ArrayList<Film> films=model.getBestRatedFilms(max);
+	toRet.put("films", films);
+	toRet.put("address", "bestRated.jsp"); //TODO es posible que haya que cambiar el nombre de la vista
+	return toRet;
     }
 }
--- MFF/src/java/MFF/Model/RSManagement.java
+++ MFF/src/java/MFF/Model/RSManagement.java
@@ -1,11 +1,18 @@
 
 package MFF.Model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Antonio Sánchez Perea
  * @date 13-dic-2011
  */
 public class RSManagement {
-    
+    public RSManagement() {
+	
+    }
+    public ArrayList<Film> getBestRatedFilms(int n) {
+	return null;
+    }
 }
