786a5e7773578cf24e33958c7c201a3826aefa1a
asanchez <asanchez@plutec.net>
2011-12-19 10:43:28 +0100

    Clase DuplicateUser añadida

---
+++ MFF/src/java/MFF/Exceptions/DuplicateUser.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package MFF.Exceptions;
+
+/**
+ *
+ * @author Antonio Sánchez Perea
+ * @date 16-dic-2011
+ */
+public class DuplicateUser extends Exception{}
